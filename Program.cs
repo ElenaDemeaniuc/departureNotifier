262287d9e4a7e2534ce3dea605f79b57b3ce2791
ElenaDemeaniuc <elena.demeaniuk@gmail.com>
2022-05-04 16:06:30 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+
+
+namespace WebAPIClient
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private static async Task ProcessRepositories()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var stringTask = client.GetStringAsync("https://apiv2.oroszi.net/elvira?from=Kelenfold&to=Erd&date=2022.05.02");
+
+            var msg = await stringTask;
+            Console.Write(msg);
+        }
+
+        static async Task Main(string[] args)
+        {
+            await ProcessRepositories();
+
+        }
+    }
+}
+
