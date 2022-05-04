2cd9ea41255ec7536d11e56ce79b0edf74f07cc7
ElenaDemeaniuc <elena.demeaniuk@gmail.com>
2022-05-04 16:24:44 +0200

    Update query parameters

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 using System.Threading.Tasks;
 using System.Net.Http;
 using System.Net.Http.Headers;
-
+using Microsoft.AspNetCore.WebUtilities;
 
 
 namespace WebAPIClient
@@ -13,11 +13,21 @@ namespace WebAPIClient
         private static async Task ProcessRepositories()
         {
             client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
-            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+            //client.DefaultRequestHeaders.Accept.Add(
+            //    new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            //client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
 
-            var stringTask = client.GetStringAsync("https://apiv2.oroszi.net/elvira?from=Kelenfold&to=Erd&date=2022.05.02");
+            var query = new Dictionary<string, string>()
+            {
+                ["from"] = "Kelenfold",
+                ["to"] = "Erd",
+                ["date"] = "2022.05.02"
+            };
+
+            var uri = QueryHelpers.AddQueryString("https://apiv2.oroszi.net/elvira", query);
+
+
+            var stringTask = client.GetStringAsync(uri);
 
             var msg = await stringTask;
             Console.Write(msg);
