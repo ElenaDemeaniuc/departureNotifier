07c084ce04ed8d8299c3e61248e44216cd0f48f5
ElenaDemeaniuc <elena.demeaniuk@gmail.com>
2022-05-25 17:43:57 +0200

    Update time calculations and read file

--- C#/Program.cs
+++ C#/Program.cs
@@ -12,36 +12,56 @@ namespace WebAPIClient
 {
     class Program
     {
-
-
         private static readonly HttpClient client = new HttpClient();
+
+
         private static async Task ProcessRepositories()
         {
-            DateTime UnixTimeBegin = new DateTime(1970, 1, 1);
             DateTime CurrentTime = DateTime.Now;
             string DateToday = CurrentTime.ToString("yyyy.MM.dd");
-            //Console.WriteLine(CurrentTime);
-            //Console.WriteLine("Debug time: " + CurrentTime.ToString("yyyy.MM.dd"));
+            string TimeNow = CurrentTime.ToString("HH:mm:ss");
+            int TimeNowMillis = StringToMillis(DateToday, TimeNow);
 
-            long StringToUnix(string InputDateToday, string inputTime)
+            //  DateTimeOffset valueString4 = Convert.ToDateTime(DateToday);
+            Console.WriteLine(CurrentTime);
+            Console.WriteLine("Debug time: " + DateToday);
+            Console.WriteLine(TimeNowMillis);
+            // Console.WriteLine(valueString4);
+
+            int StringToMillis(string InputDateToday, string inputTime)
             {
-                //     if (inputTime is null)
-                //     {
-                //         return 0;
-                //        // throw new ArgumentNullException(nameof(inputTime));
-                //     }
-                string valueString = $"{InputDateToday} {inputTime}";
-                DateTimeOffset valueDateTime = Convert.ToDateTime(valueString); //ofset because GMT     
-                TimeSpan PassedTime = valueDateTime.Subtract(UnixTimeBegin);
-                long valueInMillisecons = (long)PassedTime.TotalMilliseconds;
+                string valueString = $"{InputDateToday}";
+                string valueString2 = $"{InputDateToday} {inputTime}";
+                DateTimeOffset valueDateTime = Convert.ToDateTime(valueString); //ofset because GMT    
+                DateTimeOffset valueDateTime2 = Convert.ToDateTime(valueString2);
+                TimeSpan PassedTime = valueDateTime2.Subtract(valueDateTime);
+                int valueInMillisecons = (int)PassedTime.TotalMilliseconds;
                 return valueInMillisecons;
             }
 
 
+
+            Dictionary<string, int> homeToStation = new Dictionary<string, int>();
+            var array = File.ReadAllLines("StationText.txt");
+            for (var i = 0; i < array.Length; i += 2)
+            {
+                int a = int.Parse(array[i + 1]);
+                homeToStation.Add(array[i], a);
+            }
+
+            foreach (KeyValuePair<string, int> kvp in homeToStation)
+            {
+                Console.WriteLine("Key = {0}, Value = {1}",
+                    kvp.Key, kvp.Value);
+            }
+
+
+
+
             var query = new Dictionary<string, string>()
             { //%2A
-                ["from"] = "Erd*",
-                ["to"] = "Budapest",
+                ["from"] = "Budapest*",
+                ["to"] = "Wien*",
                 ["date"] = DateToday
                 //["date"] = "2022.05.23"
             };
@@ -53,28 +73,26 @@ namespace WebAPIClient
             //foreach (var tr in schedule.timetable)
             //    Console.WriteLine($"Start> {tr.start} Dest> {tr.destination} start time> {tr.starttime} arrival> {tr.destinationtime}");
 
+
+
+
+
             List<Train> listOfTrains = new List<Train>();
 
             foreach (var tr in schedule.timetable)
             {
-                long starting = StringToUnix(DateToday, tr.starttime);
-                long ending = StringToUnix(DateToday, tr.destinationtime);
-                listOfTrains.Add(new Train(tr, starting, ending));
+                int starting = StringToMillis(DateToday, tr.starttime);
+                int ending = StringToMillis(DateToday, tr.destinationtime);
+                if (starting > TimeNowMillis + 60000) //current time plus 1 minute
+                    listOfTrains.Add(new Train(tr, starting, ending));
             }
             Train[] arrayOfTrains = listOfTrains.ToArray();
             foreach (var trainn in arrayOfTrains)
                 Console.WriteLine($"Start: {trainn.startStation} at {trainn.StartTimeUnix}, End: {trainn.endStation} at {trainn.EndTimeUnix}, {trainn.TimeTillStationMinutes}");
 
-
-            // string valueString = $"{DateToday} 18:40";
-            // DateTimeOffset valueDateTime = Convert.ToDateTime(valueString); //ofset because GMT     
-            // TimeSpan PassedTime = valueDateTime.Subtract(UnixTimeBegin);
-            // long valueInMillisecons = (long)PassedTime.TotalMilliseconds;
-            // Console.WriteLine(valueInMillisecons);
-            //Console.WriteLine($"Debug:  {valueDateTime}");
-            //Console.WriteLine($"OldTime:  {UnixTimeBegin}");
-
         }
+
+
 
         static async Task Main(string[] args)
         {
