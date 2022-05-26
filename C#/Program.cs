063baf31c44a52baea7cae96e26b48f420876ae8
ElenaDemeaniuc <elena.demeaniuk@gmail.com>
2022-05-26 06:35:55 +0200

    Error check and create object Train

--- C#/Program.cs
+++ C#/Program.cs
@@ -22,12 +22,6 @@ namespace WebAPIClient
             string TimeNow = CurrentTime.ToString("HH:mm:ss");
             int TimeNowMillis = StringToMillis(DateToday, TimeNow);
 
-            //  DateTimeOffset valueString4 = Convert.ToDateTime(DateToday);
-            Console.WriteLine(CurrentTime);
-            Console.WriteLine("Debug time: " + DateToday);
-            Console.WriteLine(TimeNowMillis);
-            // Console.WriteLine(valueString4);
-
             int StringToMillis(string InputDateToday, string inputTime)
             {
                 string valueString = $"{InputDateToday}";
@@ -40,42 +34,43 @@ namespace WebAPIClient
             }
 
 
-
             Dictionary<string, int> homeToStation = new Dictionary<string, int>();
             var array = File.ReadAllLines("StationText.txt");
-            for (var i = 0; i < array.Length; i += 2)
+            for (var i = 0; i < array.Length; i++)
             {
-                int a = int.Parse(array[i + 1]);
-                homeToStation.Add(array[i], a);
+                string[] subs = array[i].Split(':');
+                if (String.IsNullOrEmpty(subs[1]))
+                    Console.WriteLine("File content error, time value is missing");
+                try
+                {
+                    int a = int.Parse(subs[1]);
+                    homeToStation.Add(subs[0], a); //subs[0] should be in hungarian letters. Copy-paste from website
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"Unable to parse '{subs[1]}' for '{subs[0]}'");
+                }
             }
 
-            foreach (KeyValuePair<string, int> kvp in homeToStation)
-            {
-                Console.WriteLine("Key = {0}, Value = {1}",
-                    kvp.Key, kvp.Value);
-            }
-
-
-
+            // foreach (KeyValuePair<string, int> kvp in homeToStation)
+            // {
+            //     Console.WriteLine("Key = {0}, Value = {1}",
+            //         kvp.Key, kvp.Value);
+            // }
 
             var query = new Dictionary<string, string>()
-            { //%2A
-                ["from"] = "Budapest*",
-                ["to"] = "Wien*",
-                ["date"] = DateToday
-                //["date"] = "2022.05.23"
+            {
+                ["from"] = "Budapest*",    //Can use %2A for asterisk
+                ["to"] = "Erd*",
+                ["date"] = DateToday       //["date"] = "2022.05.23" correct date format         
             };
             var uri = QueryHelpers.AddQueryString("https://apiv2.oroszi.net/elvira", query); //building uri
 
             var streamTask = client.GetStreamAsync(uri);
             var schedule = await JsonSerializer.DeserializeAsync<Schedule>(await streamTask); //deserialize Json
 
-            //foreach (var tr in schedule.timetable)
-            //    Console.WriteLine($"Start> {tr.start} Dest> {tr.destination} start time> {tr.starttime} arrival> {tr.destinationtime}");
-
-
-
-
+            // foreach (var tr in schedule.timetable)
+            //     Console.WriteLine($"Start> {tr.start} Dest> {tr.destination} start time> {tr.starttime} arrival> {tr.destinationtime}");
 
             List<Train> listOfTrains = new List<Train>();
 
@@ -83,16 +78,29 @@ namespace WebAPIClient
             {
                 int starting = StringToMillis(DateToday, tr.starttime);
                 int ending = StringToMillis(DateToday, tr.destinationtime);
-                if (starting > TimeNowMillis + 60000) //current time plus 1 minute
+                if (starting > TimeNowMillis + 60000 && starting < TimeNowMillis + 60000 * 360) //current time plus 1 minute
                     listOfTrains.Add(new Train(tr, starting, ending));
             }
+
             Train[] arrayOfTrains = listOfTrains.ToArray();
             foreach (var trainn in arrayOfTrains)
-                Console.WriteLine($"Start: {trainn.startStation} at {trainn.StartTimeUnix}, End: {trainn.endStation} at {trainn.EndTimeUnix}, {trainn.TimeTillStationMinutes}");
+            {
+                int minutetemp;
+                bool hasValue = homeToStation.TryGetValue(trainn.startStation, out minutetemp);
+                if (hasValue)
+                {
+                    trainn.TimeTillStationMinutes = minutetemp;
+                    trainn.TimeToLeaveHome = trainn.StartTimeMillis - minutetemp * 60000;
+                }
+                else
+                {
+                    Console.WriteLine("Key not present");
+                }
 
+                Console.WriteLine($"Start: {trainn.startStation} at {trainn.StartTimeMillis}, End: {trainn.endStation} at {trainn.EndTimeMillis}, {trainn.TimeTillStationMinutes}, Leave home at: {trainn.TimeToLeaveHome}");
+                // Console.WriteLine($"{trainn.TimeTillStationMinutes}, {trainn.TimeToLeaveHome}");
+            }
         }
-
-
 
         static async Task Main(string[] args)
         {
--- C#/Train.cs
+++ C#/Train.cs
@@ -7,24 +7,28 @@ namespace WebAPIClient
     {
         public string? startStation { get; set; }
         public string? endStation { get; set; }
-        public long StartTimeUnix { get; set; }
-        public long EndTimeUnix { get; set; }
-        // public string? StartTimeString { get; set; }
-        // public string? EndTimeString { get; set; }
+        public int StartTimeMillis { get; set; }
+        public int EndTimeMillis { get; set; }
         public string? TimeOnRoad { get; set; }
         public int TimeTillStationMinutes { get; set; }
-        public long TimeToLeaveHome { get; set; }
+        public int TimeToLeaveHome { get; set; }
         public Train() { }
-        public Train(TimetableItem input, long startinput, long endinput)
+        public Train(TimetableItem input, int startinput, int endinput)
         {
             startStation = input.start;
             endStation = input.destination;
             TimeOnRoad = input.totaltime;
-            StartTimeUnix = startinput;
-            EndTimeUnix = endinput;
+            StartTimeMillis = startinput;
+            EndTimeMillis = endinput;
             TimeTillStationMinutes = 0;
             TimeToLeaveHome = 0;
         }
+
+        // public int GetTimeToLeaveHome()
+        // {
+        //     TimeToLeaveHome = StartTimeMillis - TimeTillStationMinutes * 60000;
+        //     return TimeToLeaveHome;
+        // }
 
 
     }
