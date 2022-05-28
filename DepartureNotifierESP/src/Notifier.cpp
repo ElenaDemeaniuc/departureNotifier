bc60379ce4da5ab2ccb929b23d833c49197f3525
ElenaDemeaniuc <elena.demeaniuk@gmail.com>
2022-05-28 15:17:28 +0200

    Add Notifier class

--- DepartureNotifierESP/src/Notifier.cpp
+++ DepartureNotifierESP/src/Notifier.cpp
@@ -1,1 +1,52 @@
 #include <Arduino.h>
+#include "Notifier.h"
+
+Notifier::Notifier()
+{
+}
+
+void Notifier::SetTimer(int a)
+{
+    int b = a;
+    timer_remain = timer_remain + b;
+}
+
+void Notifier::Notifier_loop()
+{
+    unsigned long currenttime = millis();
+    if (currenttime == lasttime)
+    {
+        return;
+    }
+    lasttime = currenttime;
+    timerCounter++;
+    if (timerCounter == 1000)
+    {
+        timerCounter = 0;
+        beepRequest = false;
+        if (timer_remain < 0)
+        {
+            timer_remain = 0;
+        }
+        if (timer_remain != 0)
+        {
+
+            timer_remain = timer_remain - 1;
+            if (timer_remain == 0)
+            {
+                beepRequest = true;
+            }
+        }
+    }
+}
+
+int Notifier::GetTimer()
+{
+    int f = timer_remain;
+    return f;
+}
+
+bool Notifier::GetBeep()
+{
+    return beepRequest;
+}
--- DepartureNotifierESP/src/Notifier.h
+++ DepartureNotifierESP/src/Notifier.h
@@ -1,1 +1,22 @@
+#ifndef _NOTIFIER_H_
+#define _NOTIFIER_H_
+
 #include <Arduino.h>
+
+class Notifier
+{
+private:
+    int timerCounter = 0;
+    int timer_remain = 0;
+    unsigned long lasttime = 0;
+    bool beepRequest;
+
+public:
+    Notifier();
+    void Notifier_loop();
+    void SetTimer(int t);
+    int GetTimer();
+    bool GetBeep();
+};
+
+#endif
