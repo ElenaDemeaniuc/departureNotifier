3eef456762c105f0deb141f5933c511510789c23
ElenaDemeaniuc <elena.demeaniuk@gmail.com>
2022-05-29 09:40:31 +0200

    Fix string parsing in ESP

--- DepartureNotifierESP/src/main.cpp
+++ DepartureNotifierESP/src/main.cpp
@@ -23,7 +23,8 @@ const int threshold = 1500;
 unsigned long ElapsedTime;
 unsigned long lasttime = 0;
 unsigned long lasttime2 = 0;
-int32 TimeFromData;
+// int32 TimeFromData;
+bool StartDataLoop = false;
 
 int btnTime = 0;
 bool btn;
@@ -42,20 +43,21 @@ bool NotifyBuzzer2;
 
 String CityArray[3] = {"Budapest", "Wien", "Erd"};
 
-String data[] = {"Budapest;Erd;11:50;40800000;Budapest-Deli;Erd felso;12:14;0:24",
-                 "Budapest;Erd;12:05;41700000;Budapest-Deli;Erd also;12:22;0:17",
-                 "Budapest;Erd;12:10;42000000;Budapest-Deli;Erd also;12:26;0:16",
-                 "Budapest;Erd;11:57;41520000;Kobanya-Kispest;Erd also;12:34;0:14",
-                 "Erd;Budapest;14:29;51240000;Erd also;Budapest-Deli;14:49;0:20",
-                 "Erd;Budapest;14:42;51720000;Erd felso;Budapest-Deli;15:09;0:27",
-                 "Erd;Budapest;14:54;52440000;Erd felso;Kobanya-Kispest;15:31;0:37",
-                 "Erd;Budapest;15:20;54300000;Erd also;Kobanya-Kispest;16:01;0:41",
-                 "Wien;Budapest;12:42;44820000;Wien Hbf;Budapest-Keleti;15:19;2:37",
-                 "Wien;Budapest;13:25;46200000;Wien Meidling;Budapest-Keleti;16:19;2:39",
-                 "Wien;Budapest;14:37;51720000;Wien Hbf;Budapest-Keleti;17:19;2:42"};
-
 String startcity = "Start";
 String endcity = "End";
+String datastartplace = "Empty";
+String datastarttime = "Empty";
+String dataendplace = "Empty";
+String dataendtime = "Empty";
+String datatotaltime = "Empty";
+String dataleavehome = "Empty";
+
+String a = "Empty";
+String b = "Empty";
+String c = "Empty";
+String d = "Empty";
+String e = "Empty";
+String f = "Empty";
 
 Rotary Encoder1(S1, S2, Key);
 Notifier Notify;
@@ -76,10 +78,26 @@ void TimerOnDisplay(int input)
   }
 }
 
+String data[] = {"Budapest;Erd;11:50;40800000;Budapest-Deli;Erd felso;12:14;0:24",
+                 "Budapest;Erd;12:05;41700000;Budapest-Deli;Erd also;12:22;0:17",
+                 "Budapest;Erd;12:10;42000000;Budapest-Deli;Erd also;12:26;0:16",
+                 "Budapest;Erd;11:57;41520000;Kobanya-Kispest;Erd also;12:34;0:14",
+                 "Erd;Budapest;14:29;51240000;Erd also;Budapest-Deli;14:49;0:20",
+                 "Erd;Budapest;14:42;51720000;Erd felso;Budapest-Deli;15:09;0:27",
+                 "Erd;Budapest;14:54;52440000;Erd felso;Kobanya-Kispest;15:31;0:37",
+                 "Erd;Budapest;15:20;54300000;Erd also;Kobanya-Kispest;16:01;0:41",
+                 "Wien;Budapest;12:42;44820000;Wien Hbf;Budapest-Keleti;15:19;2:37",
+                 "Wien;Budapest;13:25;46200000;Wien Meidling;Budapest-Keleti;16:19;2:39",
+                 "Wien;Budapest;14:37;51720000;Wien Hbf;Budapest-Keleti;17:19;2:42"};
+int datasize = 11; // example for testing
+
+String big[11];
+int TimeFromData = 38171000; // example for testing
 //
 //
 //
 //
+String My_S = "11:50;40800000;Budapest-Deli;Erd felso;12:14;0:24 ";
 
 void setup()
 {
@@ -91,9 +109,9 @@ void setup()
 
 void loop()
 {
+
   // put your main code here, to run repeatedly:
   Encoder1.Rotary_loop();
-  TimeFromData = 42171000; // example for testing
 
   ElapsedTime = millis();
   if (ElapsedTime - lasttime > 200)
@@ -124,8 +142,14 @@ void loop()
         else if (pressTime >= thresholdReset)
         {
           Display_Reset();
-          String startcity = "Start";
-          String endcity = "End";
+          startcity = "Start";
+          endcity = "End";
+          a = "Empty";
+          b = "Empty";
+          c = "Empty";
+          d = "Empty";
+          e = "Empty";
+          f = "Empty";
           Serial.println("Press is long");
           Notify.ResetTimer();
         }
@@ -191,6 +215,7 @@ void loop()
           endcity = CityArray[index2];
           Display_EndPoint(endcity);
           Shortpress = false;
+          StartDataLoop = true;
         }
         else
         {
@@ -199,7 +224,59 @@ void loop()
       }
     }
 
-        // at the end of first loop
+    if (!startcity.equals("Start") && !endcity.equals("End") && StartDataLoop == true)
+    {
+      for (int i = 0; i < datasize; i++)
+      {
+        big[i] = data[i];
+        big[i].remove(0, startcity.length() + 1);
+        big[i].remove(0, endcity.length());
+      }
+      int index = My_S.indexOf(';');
+      if (index != 0)
+      {
+        if (a.equals("Empty"))
+        {
+          a = My_S.substring(0, index);
+          Serial.println(a);
+        }
+        else if (b.equals("Empty"))
+        {
+          b = My_S.substring(0, index);
+          Serial.println(b);
+          timing = b.toInt() - TimeFromData;
+        }
+        else if (c.equals("Empty"))
+        {
+          c = My_S.substring(0, index);
+          Serial.println(c);
+        }
+        else if (d.equals("Empty"))
+        {
+          d = My_S.substring(0, index);
+          Serial.println(d);
+        }
+        else if (e.equals("Empty"))
+        {
+          e = My_S.substring(0, index);
+          Serial.println(e);
+        }
+        else if (f.equals("Empty"))
+        {
+          f = My_S;
+          Serial.println(f);
+          StartDataLoop = false;
+        }
+      }
+      My_S.remove(0, index + 1);
+      Display_StartTimeObject(a);
+      Display_StartPoint(c);
+      Display_EndPoint(d);
+      Display_EndTime(e);
+      Display_TimeOnRoad(f);
+    }
+
+    // at the end of first loop
     if (timing != 0)
     {
       Notify.SetTimer(timing);
@@ -223,29 +300,3 @@ void loop()
     // Buzzer_On(NotifyBuzzer2);
   }
 }
-
-// String sub1 = "Budapest-Deli";
-// String sub2 = "Erd felso";
-// String sub3 = "11:50";
-// String sub4 = "12:14";
-// String sub5 = "0:24";
-// int time1 = 45600000;
-// int timenow = 42171000;
-// int timer = time1 - timenow;
-// int seconds = (timer / 1000) % 60;
-// int minutes = ((timer / (1000 * 60)) % 60);
-// int hours = ((timer / (1000 * 60 * 60)) % 24);
-
-// Serial.println("Press is long");
-// Display_timer(hours, minutes, seconds);
-// Display_StartPoint(sub1);
-// Display_EndPoint(sub2);
-// Display_TimeOnRoad(sub5);
-// Display_EndTime(sub4);
-// Display_StartTimeObject(sub3);
-// A long press of approximately 3 seconds would be changing stove number
-
-// string a is "3940000", use a.toInt()
-//  int seconds = (int) (milliseconds / 1000) % 60 ;
-//  int minutes = (int)((milliseconds / (1000 * 60)) % 60);
-//  int hours = (int)((milliseconds / (1000 * 60 * 60)) % 24);
